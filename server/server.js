2333a75772b8c7236dc8d827d0192cc2cf447e76
Roy Kincaid <roy@kincaid.ninja>
2019-01-25 06:03:47 -0800

    refactored some routes to async/await. delete token, post login, post users, delete todos

--- server/server.js
+++ server/server.js
@@ -4,10 +4,10 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
-var {authenticate} = require('./middleware/authenticate');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 
@@ -32,7 +32,7 @@ app.get('/todos', authenticate, (req, res) => {
     Todo.find({
         _creator: req.user._id
     }).then((todos) => {
-        res.send({todos});
+        res.send({ todos });
     }, (e) => {
         res.status(400).send(e);
     });
@@ -42,7 +42,7 @@ app.get('/todos', authenticate, (req, res) => {
 app.get('/todos/:id', authenticate, (req, res) => {
     //get the id
     var id = req.params.id;
-    
+
     // send back request in body
     // res.send(req.params);
 
@@ -54,114 +54,127 @@ app.get('/todos/:id', authenticate, (req, res) => {
     Todo.findOne({
         _id: id,
         _creator: req.user._id
-        }).then((todo) => {
-            if (!todo) {
+    }).then((todo) => {
+        if (!todo) {
             // if no todo, return 404 with no body
-                return res.status(404).send();
-            }
+            return res.status(404).send();
+        }
         //if todo, send back todo in body
-        res.send({todo});
+        res.send({ todo });
     }).catch((e) => {
         //if error, send 400 with no body
         res.status(400).send();
     });
 });
 
-app.delete('/todos/:id', authenticate, (req, res) => {
-    //get the id
-    var id = req.params.id;
+app.delete('/todos/:id', authenticate, async (req, res) => {
+    const id = req.params.id;
 
     //validate the id. If not, return 404
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).send();
     }
-    //remove todo by id and send doc back
-    Todo.findOneAndRemove({
-        _id: id,
-        _creator: req.user._id
-    }).then((todo) => {
+
+    try {
+        const todo = await Todo.findOneAndRemove({
+            _id: id,
+            _creator: req.user._id
+        });
         if (!todo) {
-            //If error send 400 no body
             res.status(404).send();
         }
 
-        res.send({todo});
-    }).catch((e) => {
+        res.send({ todo });
+
+    } catch (e) {
         res.status(400).send();
-    });
+    }
 });
 
 app.patch('/todos/:id', authenticate, (req, res) => {
     var id = req.params.id;
     var body = _.pick(req.body, ['text', 'completed']);
-  
+
     if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).send();
+        return res.status(404).send();
     }
-  
+
     if (_.isBoolean(body.completed) && body.completed) {
-      body.completedAt = new Date().getTime();
+        body.completedAt = new Date().getTime();
     } else {
-      body.completed = false;
-      body.completedAt = null;
+        body.completed = false;
+        body.completedAt = null;
     }
-  
+
     Todo.findOneAndUpdate({
         _id: id,
-        _creator: req.user.id},
-        {$set: body}, {new: true}).then((todo) => {
-      if (!todo) {
-        return res.status(404).send();
-      }
-  
-      res.send({todo});
-    }).catch((e) => {
-      res.status(400).send();
-    })
+        _creator: req.user.id
+    },
+        { $set: body }, { new: true }).then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            res.send({ todo });
+        }).catch((e) => {
+            res.status(400).send();
+        })
 });
 
 //POST /users
-app.post('/users', (req, res) => {
-    var body = _.pick(req.body, ['email', 'password']);
-    var user = new User(body);
-    user.save().then(() => {
-        return user.generateAuthToken();
-    }).then((token) => {
+app.post('/users', async (req, res) => {
+
+    try {
+        const body = _.pick(req.body, ['email', 'password']);
+        const user = new User(body);
+
+        await user.save();
+        const token = await user.generateAuthToken();
         res.header('x-auth', token).send(user);
-    }).catch((e) => {
+
+    } catch (e) {
         res.status(400).send(e);
-    });
+    }
 });
 
-app.get('/users/me', authenticate ,(req, res) => {
+//     user.save().then(() => {
+//         return user.generateAuthToken();
+//     }).then((token) => {
+//         res.header('x-auth', token).send(user);
+//     }).catch((e) => {
+//         res.status(400).send(e);
+//     });
+// });
+
+app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
 // POST /users/login
 // {email, password} Use bcrypt compare to compare plaintext password with hashed value
-app.post('/users/login', (req, res) => {
+app.post('/users/login', async (req, res) => {
 
-    var body = _.pick(req.body, ['email', 'password']);
-
-    User.findByCredentials(body.email, body.password).then((user) => {
-        return user.generateAuthToken().then((token) => {
-            res.header('x-auth', token).send(user);
-        });
-    }).catch((e) => {
+    try {
+        const body = _.pick(req.body, ['email', 'password']);
+        const user = await User.findByCredentials(body.email, body.password);
+        const token = await user.generateAuthToken();
+        res.header('x-auth', token).send(user);
+    } catch (e) {
         res.status(400).send();
-    });
+    }
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-    req.user.removeToken(req.token).then(() => {
+app.delete('/users/me/token', authenticate, async (req, res) => {
+    try {
+        await req.user.removeToken(req.token);
         res.status(200).send();
-    }, () => {
+    } catch (e) {
         res.status(400).send();
-    });
+    }
 });
 
 app.listen(port, () => {
     console.log(`Started on Port ${port}`);
 });
 
-module.exports = {app};
+module.exports = { app };
